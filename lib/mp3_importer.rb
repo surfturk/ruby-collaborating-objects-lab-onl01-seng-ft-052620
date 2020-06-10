2d43281d0868e943a63ac9949d4d36b69dc68397
Berkley Brown <berkleydeane@gmail.com>
2020-06-10 14:40:09 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,14 +1,10 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path, :files
   
   def initialize(path)
     @path = path
     
   end  
-  
-  def files
-    
-    
-  end  
+ 
   
 end  
