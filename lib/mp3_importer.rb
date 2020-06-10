568963b6d6c1b07035ce274add0d16f19d8140b4
Berkley Brown <berkleydeane@gmail.com>
2020-06-10 14:53:47 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,12 +2,13 @@ class MP3Importer
   attr_accessor :path, :files
   
   def initialize(path)
-    binding.pry
+    
     @path = path
     @files = []
     Dir.foreach(@path) do |file|
       if file.include?("mp3") 
         @files << file
+        binding.pry
     end
   end  
  end 
